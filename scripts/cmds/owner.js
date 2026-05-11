2df5c4cfc02ea4f82e2a27e88a6c6615761f7608
TXRS Zone <nuruzaman8519@gmail.com>
2026-05-11 16:23:28 +0600

    Update owner.js

--- scripts/cmds/owner.js
+++ scripts/cmds/owner.js
@@ -1,60 +1,70 @@
-const axios = require("axios");
-const request = require("request");
 const fs = require("fs-extra");
+const axios = require("axios");
+const path = require("path");
 const moment = require("moment-timezone");
 
-module.exports.config = {
+module.exports = {
+  config: {
     name: "owner",
     version: "2.0.0",
-    hasPermssion: 0,
-    credits: "OMOR TE", // don't change my credit!
-    description: "Show Owner Info",
-    commandCategory: "info",
-    usages: "[/owner]",
-    cooldowns: 4
-};
+    author: "OMOR TE",
+    countDown: 4,
+    role: 0,
+    shortDescription: "Show Owner Info",
+    longDescription: "Display bot owner information with image",
+    guide: "{p}owner",
+    category: "info"
+  },
 
-module.exports.run = async function({ api, event }) {
+  onStart: async function ({ message, args }) {
     const time = moment().tz("Asia/Dhaka").format("DD/MM/YYYY hh:mm:ss A");
 
     // Random image from this array
     const images = [
-        "https://graph.facebook.com/100071151280531/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662",
-        "https://i.postimg.cc/L6kG8BS4/received-1875128426597909.png",
-        "https://i.postimg.cc/7ZxdGGP3/received-1258556092530363.png"
+      "https://graph.facebook.com/100071151280531/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662",
+      "https://i.postimg.cc/L6kG8BS4/received-1875128426597909.png",
+      "https://i.postimg.cc/7ZxdGGP3/received-1258556092530363.png"
     ];
-    
-    const randomImg = images[Math.floor(Math.random() * images.length)];
-    const filePath = __dirname + "/cache/admin_random.png";
 
-    const callback = () => api.sendMessage({
-        body: `
+    const chosenUrl = images[Math.floor(Math.random() * images.length)];
+
+    const textMsg = `
 ┏━━━━━━━━━━━━━━━━━━━━━┓
 ┃    ⚠️ 𝗢𝗪𝗡𝗘𝗥 𝗜𝗡𝗙𝗢 ⚠️      
 ┣━━━━━━━━━━━━━━━━━━━━━┫
-┃ 👤 𝐍𝐚𝐦𝐞      : Omor T.E
-┃ 🚹 𝐆𝐞𝐧𝐝𝐞𝐫    : ...
-┃ ❤️ 𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧  : ...
-┃ 🎂 𝐀𝐠𝐞       : ...
-┃ 🕌 𝐑𝐞𝐥𝐢𝐠𝐢𝐨𝐧  : ...
-┃ 🏫 𝐄𝐝𝐮𝐜𝐚𝐭𝐢𝐨𝐧 : ...
-┃ 🏡 𝐀𝐝𝐝𝐫𝐞𝐬𝐬  : ..., 𝐁𝐚𝐧𝐠𝐥𝐚𝐝𝐞𝐬𝐡
+┃ 👤 𝐍𝐚𝐦𝐞     : Omor T.E
+┃ 🚹 𝐆𝐞𝐧𝐝𝐞𝐫    : --
+┃ ❤️ 𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧   : --
+┃ 🎂 𝐀𝐠𝐞      : --
+┃ 🕌 𝐑𝐞𝐥𝐢𝐠𝐢𝐨𝐧   : --
+┃ 🏫 𝐄𝐝𝐮𝐜𝐚𝐭𝐢𝐨𝐧 : --
+┃ 🏡 𝐀𝐝𝐝𝐫𝐞𝐬𝐬  : ..., Bangladesh
 ┣━━━━━━━━━━━━━━━━━━━━━┫
-┃ 📶 Discord ID Tag : Omor.TE.16016
-┃ 📢 Discord Server: https://discord.gg/PQN4P6qSrM
-┃ 🌐 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 : https://www.facebook.com/Omor.TE.16016
+┃ 📶 Discord : Omor.TE.16016
+┃ 📢 Server  : discord.gg/PQN4P6qSrM
+┃ 🌐 Facebook: fb.com/Omor.TE.16016
 ┣━━━━━━━━━━━━━━━━━━━━━┫
-┃ 🕒 SYSTEM 𝐓𝐢𝐦𝐞:  ${time}
+┃ 🕒 Time: ${time}
 ┗━━━━━━━━━━━━━━━━━━━━━┛
-        `,
-        attachment: fs.createReadStream(filePath)
-    }, event.threadID, () => fs.unlinkSync(filePath));
+    `;
 
-    request(encodeURI(randomImg))
-        .pipe(fs.createWriteStream(filePath))
-        .on("close", callback)
-        .on("error", (err) => {
-            console.error("❌ Image download failed:", err);
-            api.sendMessage("⚠️ Could not load admin image.", event.threadID);
-        });
+    try {
+      const response = await axios({
+        method: 'get',
+        url: chosenUrl,
+        responseType: 'stream'
+      });
+
+      response.data.path = `owner_${Date.now()}.png`;
+
+      await message.reply({
+        body: textMsg,
+        attachment: response.data
+      });
+    } catch (err) {
+      console.error("Image download error:", err);
+      // If image fails, send only text
+      message.reply(textMsg);
+    }
+  }
 };
