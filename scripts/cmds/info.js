84ca0a26635b1992e7855c134ad8469efe8f211f
TXRS Zone <nuruzaman8519@gmail.com>
2026-05-11 12:44:07 +0600

    Update info.js

--- scripts/cmds/info.js
+++ scripts/cmds/info.js
@@ -10,40 +10,15 @@ module.exports = {
     author: "OMOR TE",
     countDown: 5,
     role: 0,
-    shortDescription: "Bot & Owner Info",
-    longDescription: "Show bot system info and owner details",
+    shortDescription: "Get Bot & Owner Info",
+    longDescription: "Show bot system information and owner details",
     guide: "{pn} info",
-    category: "info",
-    hide: true
+    category: "info"
   },
 
-  onStart: async function ({ message, event, users, threads, config }) {
-    const { threadID, senderID } = event;
-    const { PREFIX, BOTNAME } = config;
-    const { commands } = global.client;
-    
-    const dateNow = Date.now();
-    const time = process.uptime();
-    const hours = Math.floor(time / (60 * 60));
-    const minutes = Math.floor((time % (60 * 60)) / 60);
-    const seconds = Math.floor(time % 60);
+  onStart: async function ({ message, args }) {
+    const time = moment().tz("Asia/Dhaka").format("DD/MM/YYYY hh:mm:ss A");
 
-    // Admin list from config
-    const listAdmin = global.config.ADMINBOT || [];
-    
-    let adminList = [];
-    for (const id of listAdmin) {
-      if (id) {
-        try {
-          const name = await users.getNameUser(id);
-          adminList.push(`👤 ${name} - ${id}`);
-        } catch(e) {
-          adminList.push(`👤 ${id}`);
-        }
-      }
-    }
-
-    // Image links (your original links)
     const imageLinks = [
       "https://i.postimg.cc/0jRGknT9/FB-IMG-1744474199349.jpg",
       "https://i.postimg.cc/Y9KK7KC0/Polish-20250526-101350151.jpg",
@@ -57,20 +32,14 @@ module.exports = {
       "https://i.postimg.cc/DZcknCyY/FB-IMG-1748855075592.jpg"
     ];
 
-    const randomImg = imageLinks[Math.floor(Math.random() * imageLinks.length)];
-    const cacheDir = path.join(__dirname, "cache");
-    const filePath = path.join(cacheDir, "info_img.png");
+    const chosenUrl = imageLinks[Math.floor(Math.random() * imageLinks.length)];
 
-    if (!fs.existsSync(cacheDir)) {
-      fs.mkdirSync(cacheDir, { recursive: true });
-    }
-
-    const textMessage = `
-🍀---- Hello/Assalamu Alaikum ----🍀
+    const textMsg = `
+🍀----Hello/Assalamu Alaikum----🍀
 
 ┏━━•❅•••❈•••❈•••❅•━━┓
 
-| ${BOTNAME || "Bot"} |
+| MW Legends Bot.  |
 
 ┗━━•❅•••❈•••❈•••❅•━━┛
 
@@ -78,58 +47,61 @@ ______________________________
 
 ↓↓ 𝗕𝗢𝗧 𝗦𝗬𝗦𝗧𝗘𝗠 𝗜𝗡𝗙𝗢 ↓↓
 
-» 𝗣𝗿𝗲𝗳𝗶𝘅 𝘀𝘆𝘀𝘁𝗲𝗺: ${PREFIX}
+» 𝗣𝗿𝗲𝗳𝗶𝘅 𝘀𝘆𝘀𝘁𝗲𝗺: !
 
-» 𝗧𝗼𝘁𝗮𝗹 𝗠𝗼𝗱𝘂𝗹𝗲𝘀: ${commands.size}
+» 𝗧𝗼𝘁𝗮𝗹 𝗠𝗼𝗱𝘂𝗹𝗲𝘀: Available
 
-» 𝗣𝗶𝗻𝗴: ${Date.now() - dateNow}ms
+» 𝗣𝗶𝗻𝗴: Active
 
 ______________________________
 
 ↓↓ 𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥 𝗜𝗡𝗙𝗢 ↓↓
 
-👤 𝗡𝗔𝗠𝗘: Omor T.E
+𝗡𝗔𝗠𝗘 : Omor T.E
 
-🔗 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸: fb.com/Omor.TE.16016
+𝗢𝘄𝗻𝗲𝗿 𝗜𝗱 𝗹𝗶𝗻𝗸: https://www.facebook.com/Omor.TE.16016
 
-💬 𝗗𝗶𝘀𝗰𝗼𝗿𝗱: https://discord.gg/PQN4P6qSrM
-
-${adminList.length > 0 ? `\n👑 𝗔𝗗𝗠𝗜𝗡 𝗟𝗜𝗦𝗧:\n${adminList.join("\n")}` : ""}
+Discord Server Link: https://discord.gg/PQN4P6qSrM
 
 ______________________________
 
-⏱️ 𝗕𝗢𝗧 𝗨𝗣𝗧𝗜𝗠𝗘: ${hours}h ${minutes}m ${seconds}s
+----↓↓ 𝗕𝗢𝗧 𝗨𝗣𝗧𝗜𝗠𝗘 ↓↓----
+
+Online & Running
 
 ______________________________
 
-👥 𝗧𝗢𝗧𝗔𝗟 𝗨𝗦𝗘𝗥𝗦: ${global.data.allUserID?.length || "N/A"}
+» 𝗧𝗢𝗧𝗔𝗟 𝗨𝗦𝗘𝗥𝗦: Active
 
-💬 𝗧𝗢𝗧𝗔𝗟 𝗚𝗥𝗢𝗨𝗣𝗦: ${global.data.allThreadID?.length || "N/A"}
+» 𝗧𝗢𝗧𝗔𝗟 𝗚𝗥𝗢𝗨𝗣: Active
 
 ______________________________
 
-✨ Thanks for using ~
-🏴‍☠️ MW Legends Official Bot
+Thanks for using~ 
+†★MW Legends★† Official Facebook Messenger Bot! 
+
+--------------------------------------------------
+
+🏴‍☠️⛵⚡
+
+🕒 System Time: ${time}
     `;
 
     try {
-      // Download and send image with text
       const response = await axios({
         method: 'get',
-        url: randomImg,
+        url: chosenUrl,
         responseType: 'stream'
       });
 
-      response.data.path = `info_${Date.now()}.png`;
+      response.data.path = `info_${Date.now()}.jpg`;
 
       await message.reply({
-        body: textMessage,
+        body: textMsg,
         attachment: response.data
       });
     } catch (err) {
-      console.error("Image download error:", err);
-      // If image fails, send only text
-      message.reply(textMessage);
+      message.reply(textMsg);
     }
   }
 };
