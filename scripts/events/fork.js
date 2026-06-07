4e1f4f5d7200f5afd6d97f67950f46a7bbf1675a
TXRS Zone <nuruzaman8519@gmail.com>
2026-06-08 03:19:53 +0600

    Create fork.js

---
+++ scripts/events/fork.js
@@ -0,0 +1,31 @@
+module.exports = {
+  config: {
+    name: "autoreply",
+    version: "1.0.0",
+    author: "OMOR TE",
+    role: 0,
+    shortDescription: "Send bot's repository info",
+    category: "without prefix"
+  },
+
+  onEvent: async function ({ api, event, message }) {
+    const { body, senderID } = event;
+    
+    if (senderID === api.getCurrentUserID()) return;
+    if (!body) return;
+    
+    const msg = body.toLowerCase().trim();
+    
+    // fake fork info
+    const replies = {
+      "fork": ["⚡ MW Legends; Latest"],
+     };
+    
+    for (const [keyword, responseList] of Object.entries(replies)) {
+      if (msg === keyword || msg.includes(keyword)) {
+        const randomReply = responseList[Math.floor(Math.random() * responseList.length)];
+        return message.reply(randomReply);
+      }
+    }
+  }
+};
