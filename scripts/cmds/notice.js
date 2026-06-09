f5747c14a3527824bfd101f6eaf720b2815c6627
TXRS Zone <nuruzaman8519@gmail.com>
2026-06-09 12:17:45 +0600

    Update notice.js

--- scripts/cmds/notice.js
+++ scripts/cmds/notice.js
@@ -32,7 +32,7 @@ module.exports = {
       userText = event.messageReply.body;
     }
     
-    const notificationMessage = `📢 𝗡𝗢𝗧𝗜𝗖𝗘 𝗙𝗥𝗢𝗠 𝗕𝗢𝗧 𝗔𝗗𝗠𝗜𝗡 ‼️
+    const notificationMessage = `𝗡𝗢𝗧𝗜𝗖𝗘 𝗙𝗥𝗢𝗠 𝗕𝗢𝗧 𝗔𝗗𝗠𝗜𝗡 ‼️
 ━━━━━━━━━━━━━━━━━━━━
 👤 𝗔𝗱𝗺𝗶𝗻: ${(await api.getUserInfo(event.senderID))[event.senderID]?.name || "Admin"}
 📝 𝗖𝗼𝗻𝘁𝗲𝗻𝘁: ${userText || "Only file attached"}
